65ba0d2b48a98882de86f529d4ecf268f51a74da
borris starodubtsev <boris.starodubtsev@adorsis.com>
2022-10-28 14:17:17 +0200

    N2 Spring Boot JPA (Hibernate)

--- src/main/java/com/example/adorsys/controller/MainController.java
+++ src/main/java/com/example/adorsys/controller/MainController.java
@@ -1,8 +1,12 @@
 package com.example.adorsys.controller;
 
+import com.example.adorsys.domain.Message;
 import com.example.adorsys.service.MessageService;
+import com.sun.istack.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -19,4 +23,16 @@ public class MainController {
         return messageService.mainScreen(model);
     }
 
+    @PostMapping("/")
+    public String add(
+            @RequestParam @NotNull String text,
+            @RequestParam @NotNull String tag,
+            Map<String, Object> model
+    ) {
+        return messageService.add(text, tag, model);
+    }
+    @PostMapping("filter")
+    public String filter(@RequestParam(required = false, defaultValue = "") String filter, Map<String, Object> model) {
+        return messageService.filter(filter, model);
+    }
 }
--- src/main/java/com/example/adorsys/service/MessageService.java
+++ src/main/java/com/example/adorsys/service/MessageService.java
@@ -1,15 +1,41 @@
 package com.example.adorsys.service;
 
+import com.example.adorsys.domain.Message;
+import com.example.adorsys.repository.MessageRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Service
 @AllArgsConstructor
 public class MessageService {
+    private final MessageRepository messageRepository;
     public String mainScreen(Map<String, Object> model) {
-        model.put("message", "Hello, Adorsys!");
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    public String add(String text, String tag,Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        Message messageSaved = messageRepository.save(message);
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    public String filter(String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.put("messages", messages);
 
         return "main";
     }

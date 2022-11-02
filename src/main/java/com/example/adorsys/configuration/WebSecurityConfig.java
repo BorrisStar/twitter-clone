702ab8346afcb01a901a52b5e0ccd76a8248d11f
borris starodubtsev <boris.starodubtsev@adorsis.com>
2022-11-02 13:15:15 +0100

    N4 One to Many

--- src/main/java/com/example/adorsys/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/adorsys/configuration/WebSecurityConfig.java
@@ -7,21 +7,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class WebSecurityConfig {
-    private final DataSource dataSource;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -43,7 +37,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
@@ -53,9 +47,8 @@ public class WebSecurityConfig {
     ) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
                 .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select username, password, active from usr where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?")
+                .and()
+                .userDetailsService(userDetailsServiceImpl)
                 .and()
                 .build();
     }

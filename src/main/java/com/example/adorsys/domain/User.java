b39a63ae7788189ce3fb2d15bb7b29f88b25100b
borris starodubtsev <boris.starodubtsev@adorsis.com>
2022-11-02 13:11:52 +0100

    N4 One to Many

--- src/main/java/com/example/adorsys/domain/User.java
+++ src/main/java/com/example/adorsys/domain/User.java
@@ -3,8 +3,11 @@ package com.example.adorsys.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
@@ -12,7 +15,7 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @Table(name = "usr")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -21,10 +24,11 @@ public class User {
     private String password;
     private boolean active;
 
-    public User(String username, String password, boolean active) {
+    public User(String username, String password, boolean active, Set<Role> roles) {
         this.username = username;
         this.password = password;
         this.active = active;
+        this.roles = roles;
     }
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
@@ -42,5 +46,29 @@ public class User {
                 ", roles=" + roles +
                 '}';
     }
-}
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
+}

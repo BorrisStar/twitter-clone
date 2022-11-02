adcd8d67e02c2158047c146d72f8b68dc52683e0
borris starodubtsev <boris.starodubtsev@adorsis.com>
2022-11-02 13:26:58 +0100

    N4

--- src/main/java/com/example/adorsys/service/MessageService.java
+++ src/main/java/com/example/adorsys/service/MessageService.java
@@ -1,9 +1,11 @@
 package com.example.adorsys.service;
 
 import com.example.adorsys.domain.Message;
+import com.example.adorsys.domain.User;
 import com.example.adorsys.repository.MessageRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -12,31 +14,28 @@ import java.util.Map;
 @AllArgsConstructor
 public class MessageService {
     private final MessageRepository messageRepository;
-    public String mainScreen(Map<String, Object> model) {
+
+    public String mainScreen(String tag, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-
-    public String add(String text, String tag,Map<String, Object> model) {
-        Message message = new Message(text, tag);
-        Message messageSaved = messageRepository.save(message);
-        Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-
-    public String filter(String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
+        if (tag != null && !tag.isEmpty()) {
+            messages = messageRepository.findByTag(tag);
         }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", tag);
 
+        return "main";
+    }
+
+    private void findAllMessages(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
+    }
 
+    public String add(String text, String tag, User user, Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
+        Message messageSaved = messageRepository.save(message);
+        System.out.println(messageSaved);
+        findAllMessages(model);
         return "main";
     }
 }

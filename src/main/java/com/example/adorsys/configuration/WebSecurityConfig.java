48fd138cf0c2b72c8c7d3dfa81c14cd085722a98
borris starodubtsev <boris.starodubtsev@adorsis.com>
2022-11-26 17:43:12 +0100

    N

--- src/main/java/com/example/adorsys/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/adorsys/configuration/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    PasswordEncoder getPasswordEncoder() {
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 

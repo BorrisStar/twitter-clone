b86d033edaa73f78b764d1d503b06a585c2625c5
borris starodubtsev <boris.starodubtsev@adorsis.com>
2022-11-26 21:34:53 +0100

    N

--- src/main/java/com/example/adorsys/dto/UserDto.java
+++ src/main/java/com/example/adorsys/dto/UserDto.java
@@ -1,25 +1,12 @@
 package com.example.adorsys.dto;
 
-
 import lombok.Data;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class UserDto {
 
-    @NotNull
-    @NotBlank
     private final String username;
-    @NotNull
-    @NotBlank
     private final String password;
-
-    @NotNull
-    @NotBlank
-    @Email
     private String email;
 
     private String activationCode;

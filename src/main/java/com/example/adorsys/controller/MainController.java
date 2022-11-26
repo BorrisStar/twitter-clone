8f2f7da99f8d10d12e3526a8c26054bcd0d475fe
borris starodubtsev <boris.starodubtsev@adorsis.com>
2022-11-26 21:50:44 +0100

    N

--- src/main/java/com/example/adorsys/controller/MainController.java
+++ src/main/java/com/example/adorsys/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.example.adorsys.controller;
 
-import com.example.adorsys.domain.Message;
 import com.example.adorsys.service.MessageService;
-import com.sun.istack.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,8 +23,8 @@ public class MainController {
 
     @PostMapping("/")
     public String add(
-            @RequestParam @NotNull String text,
-            @RequestParam @NotNull String tag,
+            @RequestParam String text,
+            @RequestParam String tag,
             Map<String, Object> model
     ) {
         return messageService.add(text, tag, model);

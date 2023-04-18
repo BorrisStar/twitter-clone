81d6d9db6114b06a77d611fb5644261b79764b88
borris starodubtsev <boris.starodubtsev@adorsis.com>
2023-04-18 17:14:11 +0200

    N15

--- src/main/java/com/example/adorsys/TwitterCloneApplication.java
+++ src/main/java/com/example/adorsys/TwitterCloneApplication.java
@@ -2,7 +2,6 @@ package com.example.adorsys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication

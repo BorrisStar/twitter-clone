2ed08cafdb02b2e68d982f152de5da23990343b7
borris starodubtsev <boris.starodubtsev@adorsis.com>
2023-05-09 17:04:42 +0200

    N16

--- src/main/java/com/example/adorsys/profiling/LogExecutionTime.java
+++ src/main/java/com/example/adorsys/profiling/LogExecutionTime.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.ANNOTATION_TYPE, ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogExecutionTime {
     long limit() default 1000L;
